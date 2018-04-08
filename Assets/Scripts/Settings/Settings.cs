5ebd86118225d0174db405a6099ad5c099cc885c
EvilWolve <derboesewolfi@googlemail.com>
2018-04-09 00:53:07 +0300

    Added basic game settings

---
+++ Assets/Scripts/Settings/Settings.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Unity.Mathematics;
+
+namespace game.settings
+{
+    public class Settings : ScriptableObject
+    {
+        public GameObject blockPrototype;
+
+        public int2 playfieldDimensions;
+
+        public float activeDropRate;
+        public float fillDropSpeed;
+
+        private static Settings instance;
+
+        public static Settings Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = Resources.Load<Settings> ("Settings");
+                }
+
+                return instance;
+            }
+        }
+    }
+}
