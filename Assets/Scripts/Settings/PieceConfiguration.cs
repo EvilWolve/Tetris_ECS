e111ed9a60161d1a2014f769c56b621dbb8eeac8
EvilWolve <derboesewolfi@googlemail.com>
2018-04-09 00:53:50 +0300

    Added piece library

---
+++ Assets/Scripts/Settings/PieceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace game.settings
+{
+    [CreateAssetMenu (fileName = "New Piece Config", menuName = "Pieces/Piece Config")]
+    public class PieceConfiguration : ScriptableObject
+    {
+        public int2[] layout;
+
+        public Color color;
+    }
+}
---
+++ Assets/Scripts/Settings/PieceLibrary.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace game.settings
+{
+    [CreateAssetMenu (fileName = "New Piece Library", menuName = "Pieces/Piece Library")]
+    public class PieceLibrary : ScriptableObject
+    {
+        public PieceConfiguration[] pieces;
+    }
+}
